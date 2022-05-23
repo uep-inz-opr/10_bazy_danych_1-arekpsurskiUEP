5dc641b59365dcf1081b0414b54da4abf2fe2e02
ArekPsurski <artopsu@gmail.com>
2022-05-23 19:44:18 +0200

    usuniecie nexta

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     with open(file, 'r') as fin:
         reader = csv.reader(fin, delimiter=",")
         headers = next(reader)
-        next(reader, None)  
+        #next(reader, None)  
         rows = [x for x in reader]
         cur.executemany("INSERT INTO polaczenia (from_subscriber, to_subscriber, datetime, duration , celltower) "
                         "VALUES ( ?, ?, ?, ?, ?);", rows)
